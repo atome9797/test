d88dd8f26fb74cf81b6c6530023662ef9024bc74
atome9797 <65073631+atome9797@users.noreply.github.com>
2024-12-14 06:21:18 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-app.set('port', express.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
    res.send('Hello, Express'); 
